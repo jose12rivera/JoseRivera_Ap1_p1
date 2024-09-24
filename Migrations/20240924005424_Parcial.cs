ff625592bac3a83f1b90d3f1aff7e33e0d29dd15
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-09-23 20:56:43 -0400

    Migracion

---
+++ Migrations/20240924005424_Parcial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JoseRivera_Ap1_p1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Parcial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registros", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registros");
+        }
+    }
+}
