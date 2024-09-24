8f0c37760b3bee1cb9e89a1a3ea1f44b78426e53
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-09-23 20:58:14 -0400

    Modelos

---
+++ Models/Registros.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoseRivera_Ap1_p1.Models;
+public class Registros
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Intentar Nuevamente")]
+    public string? Nombre { get; set; }
+}
