60dca29ed106642189c5badae23f9b2e3d712192
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-09-30 18:24:59 -0400

    Modelos de prestamos

---
+++ Models/Prestamos.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JoseRivera_Ap1_p1.Models;
+public class Prestamos
+{
+    [Key]
+    public int PrestamoId { get; set; }
+    [Required(ErrorMessage = "Intentar Nuevamente el Deudor")]
+    public string? Deudor { get; set; }
+    [Required(ErrorMessage = "Intentar Nuevamente el concepto")]
+    public decimal? Concepto  { get; set; }
+    [Required(ErrorMessage = "Intentar Nuevamente el Monto")]
+    public decimal? Monto { get; set; }
+}
