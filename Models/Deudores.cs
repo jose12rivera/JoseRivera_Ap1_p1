e50fdcf1afd15b4d4abdb77f7068bb2bda422483
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-11 20:00:17 -0400

    El Modelo Deudores

--- Models/Deudores.cs
+++ Models/Deudores.cs
@@ -1,12 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace JoseRivera_Ap1_p1.Models
+namespace JoseRivera_Ap1_p1.Models;
+public class Deudores
 {
-    public class Deudores
-    {
-        [Key]
-        public int DeudorId { get; set; }
-        [Required(ErrorMessage = "Intentar Nuevamente el Nombre")]
-        public string? Nombres { get; set; }
-    }
+    [Key]
+    public int DeudorId { get; set; }
+    [Required(ErrorMessage = "Intentar Nuevamente el Nombre")]
+    public string? Nombres { get; set; }
 }
