53a6b8c2b2c208c63a3eafc423ba57140a036a35
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-11 20:15:07 -0400

    El services de CobrosServices estan todos los metodo comentados por favor leer

--- Services/CobrosServices.cs
+++ Services/CobrosServices.cs
@@ -4,27 +4,26 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace JoseRivera_Ap1_p1.Services;
-
 public class CobrosServices
 {
     private readonly Contexto _contexto;
+    //El Metodo del contexto
     public CobrosServices(Contexto contexto)
     {
         _contexto = contexto;
     }
-
+    //El Metodo Existe
     public async Task<bool> Existe(int CobroId)
     {
         return await _contexto.Cobros.AnyAsync(c => c.CobroId == CobroId);
     }
-
+    //El Metodo Insertar
     private async Task<bool> Insertar(Cobros cobro)
     {
         _contexto.Cobros.Add(cobro);
         return await _contexto.SaveChangesAsync() > 0;
     }
-
-
+    //El Metodo Modificar el detalles
     public async Task<bool> Modificar(Cobros cobro)
     {
         var cobroExistente = await _contexto.Cobros
@@ -42,17 +41,15 @@ public class CobrosServices
                 }
             }
 
-           
             cobroExistente.CobroDetalles = cobro.CobroDetalles;
 
-          
             _contexto.Entry(cobroExistente).CurrentValues.SetValues(cobro);
             return await _contexto.SaveChangesAsync() > 0;
         }
 
         return false;
     }
-
+    //El Metodo Guardar
     public async Task<bool> Guardar(Cobros cobro)
     {
         if (!await Existe(cobro.CobroId))
@@ -60,6 +57,7 @@ public class CobrosServices
         else
             return await Modificar(cobro);
     }
+    //El Metdod eliminar
     public async Task<bool> Eliminar(int id)
     {
         var eliminado = await _contexto.Cobros
@@ -67,6 +65,7 @@ public class CobrosServices
             .ExecuteDeleteAsync();
         return eliminado > 0;
     }
+    //El Metodo Buscar agregue los include Deudor y CobroDetalle
     public async Task<Cobros?> Buscar(int id)
     {
         return await _contexto.Cobros
@@ -75,6 +74,7 @@ public class CobrosServices
             .AsNoTracking()
             .FirstOrDefaultAsync(c => c.CobroId == id);
     }
+    //El Metodo ObtenerPrestamoPorDeudorId agregue los include DeudorId 
     public async Task<Prestamos?> ObtenerPrestamoPorDeudorId(int deudorId)
     {
         return await _contexto.Prestamos
@@ -82,6 +82,7 @@ public class CobrosServices
         .OrderByDescending(p => p.PrestamoId) 
         .FirstOrDefaultAsync();
     }
+    //El Metodo en el listar de cobros el include deudor y CobroDetalles
     public async Task<List<Cobros>> Listar(Expression<Func<Cobros, bool>> Criterio)
     {
         return await _contexto.Cobros
@@ -91,13 +92,14 @@ public class CobrosServices
            .Where(Criterio)
            .ToListAsync();
     }
+    //El Metodo  listardeudores para el modelcreating
     public async Task<List<Deudores>> ListarDeudore()
     {
         return await _contexto.Deudores
             .AsNoTracking()
             .ToListAsync();
     }
-
+    //El Metodo de la lista deudores para ObtenerDeudoresConPrestamos
     public async Task<List<Deudores>> ObtenerDeudoresConPrestamos()
     {
         var deudoresConPrestamos = _contexto.Prestamos
@@ -105,7 +107,6 @@ public class CobrosServices
           .Select(p => p.Deudor)
           .Distinct()
           .ToList();
-
         return deudoresConPrestamos;
     }
 }
