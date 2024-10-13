a3aa64eb138bc613b95e994276f13bd899d76a5b
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-13 10:26:33 -0400

    El modelo Cobro

--- Models/Cobros.cs
+++ Models/Cobros.cs
@@ -15,5 +15,5 @@ public class Cobros
     public Deudores? Deudor { get; set; }
     [Required(ErrorMessage = "Intentar Nuevamente el Deudor")]
     public decimal Monto { get; set; }
-    public ICollection<CobrosDetalle> CobroDetalles { get; set; } = new List<CobrosDetalle>();
+    public ICollection<CobrosDetalle> CobrosDetalle { get; set; } = new List<CobrosDetalle>();
 }
