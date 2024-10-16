2ead830da666ff5494678b0d8c5ad87feb50ebc6
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-16 02:40:30 -0400

    DeudoresServices

---
+++ Services/DeudoresServices.cs
@@ -0,0 +1,32 @@
+﻿using JoseRivera_Ap1_p1.DAL;
+using JoseRivera_Ap1_p1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace JoseRivera_Ap1_p1.Services;
+
+public class DeudoresServices(Contexto _contexto)
+{
+    public async Task<List<Deudores>> Listar(Expression<Func<Deudores, bool>> criterio)
+    {
+        return await _contexto.Deudores
+             .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+
+    public async Task<List<Deudores>> ListarDeudores()
+    {
+        return await _contexto.Deudores
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public async Task<List<Deudores>> ObtenerDeudoresConPrestamos()
+    {
+        return await _contexto.Deudores
+            .Include(d => d.Prestamos)
+            .Where(d => d.Prestamos.Count() == 1)// Asegúrate de tener la relación definida
+            .ToListAsync();
+    }
+}
