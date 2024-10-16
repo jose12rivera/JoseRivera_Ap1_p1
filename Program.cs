ab9846b80127400f9ca0b095728df05868b7c37c
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-16 02:42:24 -0400

    Lista para el pull requests

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddScoped<PrestamosServices>();
 //La inyeccion del services
 builder.Services.AddScoped<CobrosServices>();
 
+builder.Services.AddScoped<DeudoresServices>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
