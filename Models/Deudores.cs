68a6edd48c052d1ed257a464256f8733ebf427dd
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-16 02:39:33 -0400

    El modelo Deudores le quite el required

--- Models/Deudores.cs
+++ Models/Deudores.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JoseRivera_Ap1_p1.Models;
 public class Deudores
@@ -7,4 +8,9 @@ public class Deudores
     public int DeudorId { get; set; }
     [Required(ErrorMessage = "Intentar Nuevamente el Nombre")]
     public string? Nombres { get; set; }
+    [InverseProperty("Deudor")]
+    public virtual ICollection<Cobros> Cobros { get; set; } = new List<Cobros>();
+
+    [InverseProperty("Deudor")]
+    public virtual ICollection<Prestamos> Prestamos { get; set; } = new List<Prestamos>();
 }
